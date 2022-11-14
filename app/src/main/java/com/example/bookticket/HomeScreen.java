aa1114cbcc79d9d7631af07c713c4ad569377143
MusicMan345 <107372719+MusicMan345@users.noreply.github.com>
2022-11-13 22:25:18 -0600

    Update to Movie Catalog and Home Screen

--- app/src/main/java/com/example/bookticket/HomeScreen.java
+++ app/src/main/java/com/example/bookticket/HomeScreen.java
@@ -2,13 +2,34 @@ package com.example.bookticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+    private Button btnToMovieCatalog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //Code from learntodroid.com
+        btnToMovieCatalog = findViewById(R.id.btnToMovieCatalog);
+        btnToMovieCatalog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch_activities();
+            }
+        });
+    }
+
+    //Based on code from learntodroid.com
+    private void switch_activities() {
+        Intent switchActivityIntent;
+
+        switchActivityIntent = new Intent(this, MovieCatalog.class);
+        startActivity(switchActivityIntent);
     }
 }
--- app/src/main/java/com/example/bookticket/MovieCatalog.java
+++ app/src/main/java/com/example/bookticket/MovieCatalog.java
@@ -2,10 +2,12 @@ package com.example.bookticket;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -20,6 +22,20 @@ public class MovieCatalog extends AppCompatActivity {
         setContentView(R.layout.activity_movie_catalog);
 
         recViewMovieCatalog = findViewById(R.id.recViewMovieCatalog);
+
+        ArrayList<String> film_names = new ArrayList<String>();
+
+        //TODO: Fetch names of currently showing films from the database and add to film_names
+        //These names are just examples
+        film_names.add("Movie1");
+        film_names.add("Movie2");
+        film_names.add("Movie3");
+
+        RecViewMovieCatalogAdapter catalog_adapter = new RecViewMovieCatalogAdapter();
+        catalog_adapter.set_film_names(film_names);
+
+        recViewMovieCatalog.setAdapter(catalog_adapter);
+        recViewMovieCatalog.setLayoutManager(new LinearLayoutManager(this));
     }
 }
 
@@ -28,23 +44,29 @@ class RecViewMovieCatalogAdapter extends RecyclerView.Adapter<RecViewMovieCatalo
     private ArrayList<String> film_names = new ArrayList<String>();
 
     public RecViewMovieCatalogAdapter() {
-
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_catalog_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.txtViewNameOfFilm.setText(film_names.get(position));
     }
 
     @Override
     public int getItemCount() {
         return film_names.size();
+    }
+
+    public void set_film_names(ArrayList<String> film_names) {
+        this.film_names = film_names;
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
