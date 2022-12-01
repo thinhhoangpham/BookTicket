28cbf9a7696564980f871cc1d3a0698f48d6ea16
thinhhoangpham <25382184+thinhhoangpham@users.noreply.github.com>
2022-11-30 18:00:48 -0600

    Create Movie.java

---
+++ app/src/main/java/com/example/bookticket/Movie.java
@@ -0,0 +1,18 @@
+package com.example.bookticket;
+
+public class Movie {
+    // Attributes variables
+    String name;
+    String description;
+    int rating;
+    String trailerURl;
+
+    public Movie(String name, String description, int rating, String trailerURL) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.trailerURl = trailerURL;
+    }
+
+
+}
