2c5722f7645e4a7f8fb77c558527e92a0ce3a826
mj-kael <mikmcfar@ttu.edu>
2022-12-02 16:10:30 -0600

    Payment
    It is unfinished. I'm pushing this, bc I'd like some help with the card number input.

---
+++ app/src/main/java/com/example/bookticket/Payment.java
@@ -0,0 +1,15 @@
+package com.example.bookticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Payment extends AppCompatActivity{
+    private Button btnContinue;
+    private Button btnCancelpayment;
+
+
+
+}
