3ec1dbbac1ba876c219fe5edde935d878d49630d
thinhhoangpham <25382184+thinhhoangpham@users.noreply.github.com>
2022-12-03 12:50:13 -0600

    Movie data

---
+++ app/src/main/java/com/example/bookticket/models/Movie.java
@@ -0,0 +1,57 @@
+package com.example.bookticket.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+    float rating;
+    int movieId;
+    double price;
+
+    public Movie(JSONObject jsonobject) throws JSONException {
+        posterPath = jsonobject.getString("poster_path");
+        title = jsonobject.getString("title");
+        overview = jsonobject.getString("overview");
+        rating = (float) jsonobject.getDouble("vote_average");
+        movieId = jsonobject.getInt("id");
+    }
+
+    //Empty constructor needed by parcel
+    public Movie() {
+
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJSONArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJSONArray.length(); i++) {
+            movies.add(new Movie(movieJSONArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public float getRating() {return  rating;}
+
+    public int getMovieId() {return movieId;}
+}
