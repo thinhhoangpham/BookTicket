321373b9d4f443fd1d5012a94d7a17b17ec5d58a
mj-kael <mikmcfar@ttu.edu>
2022-12-03 22:34:01 -0600

    Update PaymentScreen.java
    Created payment screen and made it change to a confirmation page

--- app/src/main/java/com/example/bookticket/PaymentScreen.java
+++ app/src/main/java/com/example/bookticket/PaymentScreen.java
@@ -2,16 +2,45 @@ package com.example.bookticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PaymentScreen extends AppCompatActivity {
-    private Button btnContinue;
-    private Button btnCancelpayment;
+    private Button buttonContinuePayment;
+    private Button buttonCancelPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_screen);
+
+        //Code from learntodroid.com
+        buttonContinuePayment = findViewById(R.id.buttonContinuePayment);
+        buttonContinuePayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: update the database with the info and credentials entered by the user
+                switch_activities();
+                finish();
+            }
+        });
+
+        buttonCancelPayment = findViewById(R.id.buttonCancelPayment);
+        buttonCancelPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void switch_activities() {
+        Intent switchActivityIntent;
+        //only switching to catalog to make sure button works haven't implemented a payment confirmation page yet
+        switchActivityIntent = new Intent(this, MovieCatalog.class);
+        startActivity(switchActivityIntent);
     }
 }
