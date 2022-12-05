ec9edb1b2b04d90c718cfb8fb5012ae26dd94a14
Shadows5409 <elloraashish2020@gmail.com>
2022-12-05 15:31:22 -0600

    Edit Price now works

--- app/src/main/java/com/example/bookticket/EditPrice.java
+++ app/src/main/java/com/example/bookticket/EditPrice.java
@@ -2,20 +2,29 @@ package com.example.bookticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< Updated upstream
 import android.annotation.SuppressLint;
+=======
+//import android.annotation.SuppressLint;
+>>>>>>> Stashed changes
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+<<<<<<< Updated upstream
 import android.widget.TextView;
+=======
+//import android.widget.TextView;
+>>>>>>> Stashed changes
 
 
 public class EditPrice extends AppCompatActivity {
     private Button btnSubmitPrice;
     private Button backbtn;
     private EditText editTextPrice;
+<<<<<<< Updated upstream
     public int Price;
     TextView currentprice;
 
@@ -23,14 +32,26 @@ public class EditPrice extends AppCompatActivity {
 
    // @SuppressLint("MissingInflatedId")
     @SuppressLint("MissingInflatedId")
+=======
+
+    public String Price;
+   // TextView currentprice;
+
+
+
+>>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_price);
+<<<<<<< Updated upstream
         Price =7;
 
         //currentprice = findViewById(R.id.currentprice);
         //currentprice.setText(Price);
+=======
+
+>>>>>>> Stashed changes
         editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         editTextPrice.setInputType(InputType.TYPE_CLASS_NUMBER);
         backbtn = findViewById(R.id.backbtn);
@@ -44,6 +65,7 @@ public class EditPrice extends AppCompatActivity {
         btnSubmitPrice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+<<<<<<< Updated upstream
 
                 if(editTextPrice.getInputType()>0){
                     Price = editTextPrice.getInputType();
@@ -53,6 +75,18 @@ public class EditPrice extends AppCompatActivity {
             }
 
     });}
+=======
+                Intent i = new Intent(EditPrice.this, PriceComfirm.class);
+                Price = editTextPrice.getText().toString();
+                i.putExtra("Price", Price);
+                startActivity(i);
+                finish();
+                }
+
+            });
+
+        };
+>>>>>>> Stashed changes
     private void switch_activities(int next_activity) {
         Intent switchActivityIntent;
 
@@ -66,4 +100,9 @@ public class EditPrice extends AppCompatActivity {
         }
         startActivity(switchActivityIntent);
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+
+>>>>>>> Stashed changes
